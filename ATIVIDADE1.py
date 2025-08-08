c56ff1140fb815c2fe6a45aaee4173ea80a51dae
marcusbarguil <barguilmarcus@gmail.com>
2025-08-08 15:35:34 -0300

    Create ATIVIDADE1.py
    Primeira versao do codigo da atividade1 do curso sgpy

---
+++ ATIVIDADE1.py
@@ -0,0 +1,16 @@
+ester_amiga = "marcus cesar "
+marcus_idade = "28 anos"
+print(ester_amiga) + (marcus_idade)
+
+
+idade = 59
+peso = 86
+produto = idade * peso
+print ("resultado da idade  * peso")
+print produto
+
+
+modulo = peso % idade
+print ("do peso % idade")
+print modulo
+
