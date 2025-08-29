7b1941127d9ae73644b276b9c1ac38b936401ee6
marcusbarguil <barguilmarcus@gmail.com>
2025-08-29 15:22:19 -0300

    correcao atividade1

--- ATIVIDADE1.py
+++ ATIVIDADE1.py
@@ -1,16 +1,16 @@
 ester_amiga = "marcus cesar "
 marcus_idade = "28 anos"
-print(ester_amiga) + (marcus_idade)
+print(ester_amiga + marcus_idade)
 
 
 idade = 59
 peso = 86
 produto = idade * peso
 print ("resultado da idade  * peso")
-print produto
+print ( produto)
 
 
 modulo = peso % idade
 print ("do peso % idade")
-print modulo
+print (modulo)
 
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ codigos que fizemos durante o curso da studiogames
 exemplos: print, variavel, % , + , # , """ , += , int , float ,str
 Marcus antonio cesar barguil
 Uso livre
+testando
