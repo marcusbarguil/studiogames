34b7bbe2d48ef832c32104363226725274106f64
marcusbarguil <barguilmarcus@gmail.com>
2025-09-19 16:00:47 -0300

    Create ifelse.py
    atividade slides if else

---
+++ ifelse.py
@@ -0,0 +1,9 @@
+GabrielInteligente=False
+Marcusinteligente=False
+if GabrielInteligente: 
+    print("inteligente")
+else:
+    if Marcusinteligente:
+        print("inteligente")
+    else:
+        print("precisam estudar")
