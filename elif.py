6b4a8e61e083aea3becafbf66d86ee51295c97a0
marcusbarguil <barguilmarcus@gmail.com>
2025-10-17 15:13:17 -0300

    Create elif.py
    elif

---
+++ elif.py
@@ -0,0 +1,11 @@
+nota_colegio = float(input("nota "))
+if nota_colegio >= 9:
+    print("nota a")
+elif nota_colegio >=8:
+    print("nota b")
+elif nota_colegio >= 7:
+    print("nota c")
+elif nota_colegio>=6:
+    print("nota d")
+else:
+    print("nota f")
